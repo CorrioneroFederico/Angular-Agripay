710087b019f26cb22e93eb277e13b965aae96ea7
CorrioneroFederico <corrionerofederico@gmail.com>
2022-08-10 20:12:09 -0300

    Terminado

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,40 +1,34 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TokenService } from '../services/token.service';
 import { UsuarioService } from '../services/usuario.service';
 import swal from 'sweetalert2';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
 })
 export class UsuarioComponent implements OnInit {
-  //*Atributos
+
   public usuarios: any[];
   public form: FormGroup;
 
-  //*Constructor
+
   constructor(
     private fb: FormBuilder,
-    private router: Router,
     private _tokenService: TokenService,
     private _usuarioService: UsuarioService
   ) {
     this.usuarios = [];
     this.form = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      address: [''],
-      ssn: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('^[0-9]{3}[-][0-9]{2}[-][0-9]{4}'),
-        ],
-      ],
+      firstName: ['',Validators.required],
+      lastName: ['',Validators.required],
+      address: ['',Validators.required],
+      ssn: ['',[Validators.required, Validators.pattern('^[0-9]{3}[-][0-9]{2}[-][0-9]{4}')]],
     });
   }
+
+
 
   ngOnInit(): void {
     this.getToken();
@@ -60,20 +54,27 @@ export class UsuarioComponent implements OnInit {
 
   public create(event: Event): void {
     event.defaultPrevented;
-    if (this.form.valid) {
-      this.usuarios.push(this.form.value);
-      this._usuarioService.create(this.form.value).subscribe((usuario) => {
-        this.router.navigate(['/usuario']);
-        swal.fire(
-          'Cliente guardado',
-          `Usuario ${usuario.nombre} ${usuario.apellido} creado exitosamente`,
-          'success'
-        );
-      });
-    }
-  }
+    let repetido=false;
+    this.usuarios.forEach( p => p.ssn == this.form.value.ssn? repetido=true:false);
 
-  get ssnValidation() {
-    return this.form.get('ssn');
+    if (this.form.valid && !repetido) {
+      this.usuarios.push(this.form.value);
+      this._usuarioService.create(this.form.value).subscribe(
+        usuario => {
+          swal.fire(
+            'Usuario guardado',
+            `Usuario ${usuario.firstName} ${usuario.lastName} creado exitosamente`,
+            'success'
+          );
+        }
+      );
+      this.form.reset();
+    }else{
+      swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'El SSN ingresado ya se encuentra registrado'
+      })
+    }
   }
 }
